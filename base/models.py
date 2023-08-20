f057f721cdddf01cfe3740ca3d0d1c8dfc2cb5c4
Clarkliu97 <89189221+Clarkliu97@users.noreply.github.com>
2023-08-20 10:31:34 +1000

    Feat(Database): 1. Created DocumentErrorDetail table, many to one relationship with Document, cascading delete 2. Created DocumentErrorStat table, one to one relationship with Document, cascading delete 3. Added category and analysis_complete fields to Document table 4. Overrade save method in Document model to create empty DocumentErrorDetail and DocumentErrorStat objects 5. Created UserProfile table, one to one relationship with User, cascading delete
    Feat(serializers):
    1. Created DocumentUploadSerializer, which is previously DocumentSerializer, only file field is required
    2. The DocumentSerializer now is only for viewing purpose, and it has all fields of Document model
    3. Created DocumentErrorDetailSerializer and DocumentErrorStatSerializer, all fields included
    
    Feat(views): The serializers_class of DocumentUploadView is changed to DocumentUploadSerializer

--- base/models.py
+++ base/models.py
@@ -26,6 +26,75 @@ class Document(models.Model):
     last_modified = models.DateTimeField(blank=True, null=True)
     word_count = models.CharField(max_length=100, blank=True, null=True)
 
+    # document analysis results
+    category = models.CharField(max_length=100, blank=True, null=True)
+    analysis_complete = models.BooleanField(default=False)
 
     def __str__(self):
-        return "{}-{}".format(self.filename, self.body[:20])
+        return "{}".format(self.filename)
+    
+    def save(self, *args, **kwargs):
+        # Override save method to update document body from file
+        # Note: this is only called when the document is created, not when it is updated
+        #       so the user must delete the document and reupload it to update the body
+        #       this is because the body is only used for analysis, and we don't want to
+        #       parse the document every time it is updated
+        
+        # return normally
+        super().save(*args, **kwargs)
+
+        # Create empty DocumentErrorDetail and DocumentErrorStat objects
+        doc_error_detail = DocumentErrorDetail.objects.create(document=self)
+        doc_error_detail.save()
+        doc_error_stat = DocumentErrorStat.objects.create(document=self)
+        doc_error_stat.save()
+
+class DocumentErrorDetail(models.Model):
+    # Many to one relationship with Document
+    id = models.AutoField(primary_key=True)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    sentence = models.CharField(max_length=1000, default="")
+    mistake_text = models.CharField(max_length=1000, default="")
+    error_message = models.CharField(max_length=1000, default="")
+    replacement = models.CharField(max_length=1000, default="")
+    start_index = models.IntegerField(default=0)
+    end_index = models.IntegerField(default=0)
+    error_type = models.CharField(max_length=100, default="")
+    error_category = models.CharField(max_length=100, default="")
+
+    def __str__(self):
+        return "{}".format(self.document.filename)
+    
+
+class DocumentErrorStat(models.Model):
+    # One to one relationship with Document
+    id = models.AutoField(primary_key=True)
+    document = models.OneToOneField(Document, on_delete=models.CASCADE)
+    total_errors = models.IntegerField(default=0)
+    spelling_errors = models.IntegerField(default=0)
+    grammar_errors = models.IntegerField(default=0)
+    punctuation_errors = models.IntegerField(default=0)
+    syntax_errors = models.IntegerField(default=0)
+    style_errors = models.IntegerField(default=0)
+    other_errors = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "{}".format(self.document.filename)
+    
+
+class UserProfile(models.Model):
+    # One to one relationship with User
+    id = models.AutoField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    updating = models.BooleanField(default=False) # True if the user's documents are being analysed, False otherwise
+
+    def __str__(self):
+        return "{}".format(self.user.username)
+    
+# Override the default User model save method to create a UserProfile object when a User is created
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+# Connect the create_user_profile function to the post_save signal
+models.signals.post_save.connect(create_user_profile, sender=User)
